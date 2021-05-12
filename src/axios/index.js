416a5eceba308d91950e99612de5fed86e7a04d0
Alassiag <73958085+alassiag@users.noreply.github.com>
2021-05-12 17:23:19 -0300

    Update index.js

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://nuestraweb.com.ar/api/' 
+  baseURL: 'https://luiszelarrayan-back-laravel.herokuapp.com/'
 })
 
-
+//https://nuestraweb.com.ar/api/
 //https://nelson-laravel-api.herokuapp.com/public/api
 //https://laravel-api-back.herokuapp.com/public/api/
 //http://localhost/api/
