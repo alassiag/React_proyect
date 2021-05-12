717bc02fa4def7ea853a10b4674c778675634eb8
Alassiag <73958085+alassiag@users.noreply.github.com>
2021-05-12 17:05:06 -0300

    Update index.js

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://laravel-api-back.herokuapp.com/public/api/' 
+  baseURL: 'https://nelson-laravel-api.herokuapp.com/public/api' 
 })
-//https://nelson-laravel-api.herokuapp.com/public/api
+
+
+//https://laravel-api-back.herokuapp.com/public/api/
+
 //http://localhost/api/
 
 const sendEmail = async (data) => {
